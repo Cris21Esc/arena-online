7507509339f8e4a5b0889df80729e8b2067c3f9f
CRISTOPHER ALEXANDER ESCALANTE JADAN <caescalante@ccoo.es>
2026-08-04 09:40:21 +0200

    Añadidas funciones de chat global con id

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,59 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
 app.use(cors());
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
 app.get('/', (_req, res) => {
   res.send('Arena Online Backend Running');
 });
 
+//Connect
+io.on('connection', (socket) => {
+
+  console.log('User connected:', socket.id);
+
+  socket.on('ping-server',(message) => {
+
+    console.log('Mensaje recibido:', message);
+
+    socket.emit(
+      'pong-client',
+      'Hola cliente'
+    );
+
+  });
+
+  socket.on('chat-message', (message) => {
+
+    console.log('Chat:', message);
+
+    io.emit(
+      'chat-message',
+      message
+    );
+
+  });
+
+  //Disconnect
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
